4a118e2b50b0323f954f7d6809e0023f8030fca8
ArcherHeffern <heffernarcher@gmail.com>
2025-02-24 20:22:24 -0500

    c things

---
+++ c.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int func(int x) {
+	printf("Func was passed %d\n", x);
+}
+
+int main() {
+	// variable assignment
+	int x = 5;
+	int y
+	y = 5
+
+	// Calling functions
+	func(x)
+
+	// if statements
+	if (x < 10) {
+		;
+	} else if (y < 3) {
+		;
+	} else {
+		;
+	}
+
+	// for loops
+	for (int i = 0; i < 10; i++) {
+	}
+
+	// while loops
+	while (1) {
+	}
+}
